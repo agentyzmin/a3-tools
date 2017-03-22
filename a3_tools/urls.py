aa28b9d1c842ed68fb583578b303264e8724a41a
proggeo <mashtaliryurii@knu.ua>
2017-03-22 11:23:19 +0200

    add copy to clipboard buttons

--- a3_tools/urls.py
+++ a3_tools/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+import metrotimer.views
 
 admin.autodiscover()
-import metrotimer.views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
